10bf3fed54513988b365f725d190e8c6560c08a3
SatishKumar A <satishkumar.a@gmail.com>
2019-12-24 12:18:45 +0100

    spring-security-ldap

---
+++ src/test/java/com/db/cb/ses/SpringSecurityLdapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.db.cb.ses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityLdapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
